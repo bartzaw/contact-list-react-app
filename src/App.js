0e9a16f011fb6cc5c1b139e87742a34c0847dff5
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-08-19 10:48:47 +0200

    add fetch from server function

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    contacts: [],
+  };
+
+  getContacts = () => {
+    fetch('http://localhost:3000/contacts')
+      .then(response => {
+        return response.json()
+      })
+
+  };
+
+
+
   render() {
     return (
-
+      <ul id="contacts"></ul>
     );
   }
 }
