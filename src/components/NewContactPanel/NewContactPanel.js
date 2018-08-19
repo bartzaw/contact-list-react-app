e82452196d6cf0bbfe7793c2e7370d693f570fc9
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-08-19 13:24:45 +0200

    add addContact function to newcontactpanel

--- src/components/NewContactPanel/NewContactPanel.js
+++ src/components/NewContactPanel/NewContactPanel.js
@@ -11,6 +11,25 @@ class newContactPanel extends Component {
     email: '',
   }
 
+  addContact = (event) => {
+    event.preventDefault();
+    const newContactItem = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      phoneNumber: this.state.phoneNumber,
+      email: this.state.email
+    }
+    fetch(
+      'http://localhost:3001/contacts', {
+        method: 'POST',
+        body: JSON.stringify(newContactItem),
+        headers: {
+          'Content-type': 'application/json'
+        }
+      }
+    ).then(this.props.getContacts)
+  };
+
   render(){
     return (
       <div className='contacts-container'>
