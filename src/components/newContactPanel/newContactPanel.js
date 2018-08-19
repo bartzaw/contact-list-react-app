bf5b3c33f9ee35c075ccfbb5e793e7f45bd937e6
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-08-19 10:57:15 +0200

    add newContactPanel component with basis structure

---
+++ src/components/newContactPanel/newContactPanel.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import './newContactPanel.css'
+
+class newContactPanel extends Component {
+  render(){
+    return (
+
+    )
+  }
+}
+
+export default newContactPanel
