3fa14e7d4799a8c17945f44017d971cff80fce74
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-08-19 11:11:38 +0200

    add form with inputs to newContactPanel

--- src/components/newContactPanel/newContactPanel.js
+++ src/components/newContactPanel/newContactPanel.js
@@ -4,7 +4,44 @@ import './newContactPanel.css'
 class newContactPanel extends Component {
   render(){
     return (
+      <div className='contacts-container'>
+        <form className='contacts-newContact'>
 
+          <label>
+            First name:
+            <input
+              name="firstName"
+              type='text'
+            />
+          </label>
+
+          <label>
+            Last name:
+            <input
+              name="lastName"
+              type='text'
+            />
+          </label>
+
+          <label>
+            Phone number:
+            <input
+              name="phoneNumber"
+              type='number'
+            />
+          </label>
+
+          <label>
+            Email:
+            <input
+              name="emailAddress"
+              type='mail'
+            />
+          </label>
+
+          <div className='contacts-addButton' title='Add new contact'>.</div>
+        </form>
+      </div>
     )
   }
 }
