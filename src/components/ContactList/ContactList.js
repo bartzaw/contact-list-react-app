609e7d435df847faa061e2279d4d184397186e00
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-08-19 14:39:29 +0200

    add state and editpaneldisplay to contactlist

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -2,7 +2,29 @@ import React, { Component } from 'react'
 import DisplayContact from '../DisplayContact/DisplayContact'
 import './ContactList.css'
 
-class ContactsList extends Component {
+class ContactList extends Component {
+  state = {
+    editPanelDisplay: {}
+  };
+
+  displayEditPanel = (id) => {
+    this.setState(prevState => ({
+      editPanelDisplay: {
+        ...prevState.editPanelDisplay, [id]: !prevState.editPanelDisplay[id]
+      }
+    }))
+  };
+
+  removeContact = id => {
+    fetch(
+      'http://localhost:3001/contacts/' + id, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(this.props.getContacts)
+  };
 
   render() {
     const contacts = this.props.contactsReceived;
@@ -11,13 +33,17 @@ class ContactsList extends Component {
         {contacts.map(contact => {
         return (
           <div>
-            <li className='contact-list' />
+            <li className='contact-list' key={contact.id}>
               <DisplayContact
                 firstName={contact.firstName}
                 lastName={contact.lastName}
                 phoneNumber={contact.phoneNumber}
                 email={contact.email}
               />
+            <button onClick={() => this.removeContact(contact.id)}>Remove</button>
+            <button onClick={() => this.displayEditPanel(contact.id)}>Edit</button>
+
+            </li>
           </div>
         )
         })}
@@ -26,4 +52,4 @@ class ContactsList extends Component {
   }
 }
 
-export default ContactsList
+export default ContactList
