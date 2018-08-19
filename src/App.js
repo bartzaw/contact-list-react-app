2d15c04320c389444ca9416ae30848766d6e9306
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-08-19 10:51:31 +0200

    add sort function for fetched contacts

--- src/App.js
+++ src/App.js
@@ -12,9 +12,22 @@ class App extends Component {
       .then(response => {
         return response.json()
       })
-
+      .then(data => {
+        const contactList = data.sort(this.sortContacts).map(contact=>contact)
+        this.setState({
+          contacts: contactList
+        })
+      })
   };
 
+  sortContacts = (prevContact, nextContact) => {
+    if (prevContact.lastName.toLowerCase() < nextContact.lastName.toLowerCase()) {
+      return -1
+    } else if (prevContact.lastName.toLowerCase() > nextContact.lastName.toLowerCase()) {
+      return 1
+    }
+    return 0;
+  };
 
 
   render() {
