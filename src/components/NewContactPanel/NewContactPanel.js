bc3d0dfcf8c0a86524d12bf1327f005b37ca4774
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-08-24 22:11:30 +0200

    add styled components to whole newcontactpanel

--- src/components/NewContactPanel/NewContactPanel.js
+++ src/components/NewContactPanel/NewContactPanel.js
@@ -1,4 +1,41 @@
 import React, {Component} from 'react'
+import styled from 'styled-components'
+import addButton from '../images/addContact.png'
+
+const AddButton = styled.button`
+  background: url(${addButton}) no-repeat center center;
+  margin: auto;
+  height: 81px;
+  width: 81px;
+`;
+
+const NewContact = styled.form`
+    text-align: center;
+    align-self: center;
+    height: 25%;
+    display: flex;
+    flex-wrap: wrap;
+    width: 75%;
+    border: 3px solid #000;
+`;
+
+const NewContactItem = styled.div`
+    display: flex;
+    width: 50%;
+    height: 50px;
+    justify-content: center;
+`;
+
+const ContactLabel = styled.label`
+  font-weight: bold;
+  width: 125px;
+  align-self: center;
+`;
+
+const ContactListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 class newContactPanel extends Component {
 
@@ -17,7 +54,7 @@ class newContactPanel extends Component {
       phoneNumber: '',
       email: ''
     })
-  }
+  };
 
   addContact = (event) => {
     event.preventDefault();
@@ -27,7 +64,7 @@ class newContactPanel extends Component {
       lastName: this.state.lastName,
       phoneNumber: this.state.phoneNumber,
       email: this.state.email
-    }
+    };
     fetch(
       'http://localhost:3001/contacts', {
         method: 'POST',
@@ -41,59 +78,63 @@ class newContactPanel extends Component {
 
   render(){
     return (
-      <div className='contacts-container'>
-        <form className='contacts-newContact' onSubmit={this.addContact}>
-          <label>
-            First name:
-            <input
-              name="firstName"
-              type='text'
-              value={this.state.firstName}
-              onChange={(event) => this.setState(
-                {firstName: event.target.value
-                })}
-            />
-          </label>
-
-          <label>
-            Last name:
-            <input
-              name="lastName"
-              type='text'
-              value={this.state.lastName}
-              onChange={(event) => this.setState(
-                {lastName: event.target.value
-                })}
-            />
-          </label>
-
-          <label>
-            Phone number:
-            <input
-              name="phoneNumber"
-              type='number'
-              value={this.state.phoneNumber}
-              onChange={(event) => this.setState(
-                {phoneNumber: event.target.value
-                })}
-            />
-          </label>
-
-          <label>
-            Email:
-            <input
-              name="emailAddress"
-              type='mail'
-              value={this.state.email}
-              onChange={(event) => this.setState(
-                {email: event.target.value
-                })}
-            />
-          </label>
-
-          <button className='contacts-addButton' title='Add new contact'>New</button>
-        </form>
-      </div>
+      <ContactListContainer className='contacts-container'>
+        <NewContact className='contacts-newContact' onSubmit={this.addContact}>
+          <NewContactItem>
+            <ContactLabel>
+              First name:
+              <input
+                name="firstName"
+                type='text'
+                value={this.state.firstName}
+                onChange={(event) => this.setState(
+                  {firstName: event.target.value
+                  })}
+              />
+            </ContactLabel>
+          </NewContactItem>
+          <NewContactItem>
+            <ContactLabel>
+              Last name:
+              <input
+                name="lastName"
+                type='text'
+                value={this.state.lastName}
+                onChange={(event) => this.setState(
+                  {lastName: event.target.value
+                  })}
+              />
+            </ContactLabel>
+          </NewContactItem>
+          <NewContactItem>
+            <ContactLabel>
+              Phone number:
+              <input
+                name="phoneNumber"
+                type='number'
+                value={this.state.phoneNumber}
+                onChange={(event) => this.setState(
+                  {phoneNumber: event.target.value
+                  })}
+              />
+            </ContactLabel>
+          </NewContactItem>
+          <NewContactItem>
+            <ContactLabel>
+              Email:
+              <input
+                name="emailAddress"
+                type='mail'
+                value={this.state.email}
+                onChange={(event) => this.setState(
+                  {email: event.target.value
+                  })}
+              />
+            </ContactLabel>
+          </NewContactItem>
+          <AddButton title='Add new contact'></AddButton>
+        </NewContact>
+      </ContactListContainer>
     )
   }
 }
